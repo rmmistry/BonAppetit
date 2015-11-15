64a64eb2ec19d07a6d2c0b171ce52c0f550133a2
Riddhi Mistry <riddhim@riddhim-mac.local>
2015-11-14 16:41:34 -0800

    added new files

---
+++ secrets.sh
@@ -0,0 +1,14 @@
+#edamam API
+# export APP_ID="31271d33"
+# export APP_KEY="c3ca231ee08b6627f481942463afa9d2"
+
+
+
+#yummly API
+export APP_ID="555e8ec7"
+export APP_KEY="e119049bdf8251b4ec4c5abd16a89c3e"
+
+#http://api.yummly.com/v1/api/recipes?_app_id=app-id&_app_key=app-key&your _search_parameters
+
+
+
